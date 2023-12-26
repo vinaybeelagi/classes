018fe7ad5346a6ee44304fd7aa928f6f7115fdb7
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-26 19:52:13 +0530

    task-Class extends Object?

--- class_Inheritance.js
+++ class_Inheritance.js
@@ -67,3 +67,15 @@ let articles = [
   articles.sort(Article.compare);
   console.log(articles[0].title);
 
+//   task-Class extends Object?
+class Rabbit extends Object {
+    constructor(name) {
+      super(); // need to call the parent constructor when inheriting
+      this.name = name;
+    }
+  }
+  
+  let rabbit = new Rabbit("Rab");
+  
+  console.log( rabbit.hasOwnProperty('name') ); // true
+
