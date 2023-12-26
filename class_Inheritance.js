604f89833e244f3c85dddae2cf09c7d7591162af
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-26 17:28:36 +0530

    In a class declaration, they are prepended by static keyword

--- class_Inheritance.js
+++ class_Inheritance.js
@@ -1,32 +1,42 @@
-// task-class-inheritance
-class Animal {
+// // task-class-inheritance
+// class Animal {
 
-    constructor(name) {
-      this.name = name;
-    }
+//     constructor(name) {
+//       this.name = name;
+//     }
   
-  }
+//   }
   
-  class Rabbit extends Animal {
-    constructor(name) {
-      super(name)
-      this.created = Date.now();
-    }
-  }
+//   class Rabbit extends Animal {
+//     constructor(name) {
+//       super(name)
+//       this.created = Date.now();
+//     }
+//   }
   
-  let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
-console.log(rabbit.name);
+//   let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+// console.log(rabbit.name);
 
-//task-Extended clock
-class ExtendedClock extends Clock {
-    constructor(options) {
-      super(options);
-      let { precision = 1000 } = options;
-      this.precision = precision;
-    }
+// //task-Extended clock
+// class ExtendedClock extends Clock {
+//     constructor(options) {
+//       super(options);
+//       let { precision = 1000 } = options;
+//       this.precision = precision;
+//     }
   
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), this.precision);
+//     start() {
+//       this.render();
+//       this.timer = setInterval(() => this.render(), this.precision);
+//     }
+//   };
+
+//   static inheritance and mehtods
+// In a class declaration, they are prepended by static keyword, like this:
+class User{
+    static staticMethod() {
+        console.log(this === User)
     }
-  };
+}
+User.staticMethod();
+
