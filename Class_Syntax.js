5a50570e5ed73202b69edb64b4252044e1e27730
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-26 12:31:37 +0530

    task-Rewrite to class

--- Class_Syntax.js
+++ Class_Syntax.js
@@ -1,28 +1,87 @@
+// // class User {
+// //     constructor(name){
+// //         this.name = name;
+// //     }
+// //     sayHi(){
+// //         console.log(this.name);
+// //     }
+// // }
+// // let user1 = new User("beelagi")
+// // user1.sayHi();
+
+// // class declaration
 // class User {
-//     constructor(name){
-//         this.name = name;
-//     }
+//     constructor(name) { this.name = name; }
+//     sayHi() { alert(this.name); }
+//   }
+  
+//   // class is a function
+// console.log(typeof User); // function
+  
+//   // ...or, more precisely, the constructor method
+// console.log(User === User.prototype.constructor); // true
+  
+//   // The methods are in User.prototype, e.g:
+// console.log(User.prototype.sayHi); // the code of the sayHi method
+  
+//   // there are exactly two methods in the prototype
+// console.log(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+// // Class Expression
+// let user2 = class {
 //     sayHi(){
-//         console.log(this.name);
+//     console.log("hello")
 //     }
 // }
-// let user1 = new User("beelagi")
-// user1.sayHi();
 
-// class delcartion
-class User {
-    constructor(name) { this.name = name; }
-    sayHi() { alert(this.name); }
+// function makeClass(phrase) {
+//     return class {
+//         sayHi() {
+//             console.log(phrase)
+//         }
+//     };
+// }
+// let User4 = makeClass("hello");
+// new User4().sayHi();
+
+// task-Rewrite to class
+class Clock {
+    constructor({ template }) {
+      this.template = template;
+    }
+  
+    render() {
+      let date = new Date();
+  
+      let hours = date.getHours();
+      if (hours < 10) hours = '0' + hours;
+  
+      let mins = date.getMinutes();
+      if (mins < 10) mins = '0' + mins;
+  
+      let secs = date.getSeconds();
+      if (secs < 10) secs = '0' + secs;
+  
+      let output = this.template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+  
+      console.log(output);
+    }
+  
+    stop() {
+      clearInterval(this.timer);
+    }
+  
+    start() {
+      this.render();
+      this.timer = setInterval(() => this.render(), 1000);
+    }
   }
   
-  // class is a function
-console.log(typeof User); // function
   
-  // ...or, more precisely, the constructor method
-console.log(User === User.prototype.constructor); // true
+  let clock = new Clock({template: 'h:m:s'});
+  clock.start();
+  clock.stop();
   
-  // The methods are in User.prototype, e.g:
-console.log(User.prototype.sayHi); // the code of the sayHi method
-  
-  // there are exactly two methods in the prototype
-console.log(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
