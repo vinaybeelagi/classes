4960e0983f51809e107d41c690a6cfc99619ed60
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-26 15:45:16 +0530

    task-class-inheritance

---
+++ class_Inheritance.js
@@ -0,0 +1,18 @@
+// task-class-inheritance
+class Animal {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name) {
+      super(name)
+      this.created = Date.now();
+    }
+  }
+  
+  let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+console.log(rabbit.name);
