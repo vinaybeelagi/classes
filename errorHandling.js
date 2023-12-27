77062d5952b5f9b3ab48421dfad7c5b153ab749f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 18:02:37 +0530

    JSON.parse of try..catch

--- errorHandling.js
+++ errorHandling.js
@@ -5,3 +5,9 @@ try {
 catch(err) {
 //error message
 }
+
+// JSON.parse of try..catch.
+let json = '{"name":"john","age":30}';
+let user = JSON.parse(json);
+console.log(user.name);
+console.log(user.age);
