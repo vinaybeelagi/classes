646e3dd0fa5d1b91de01a281977bd1d4af276073
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 13:22:23 +0530

    Bonus: Object.prototype.toString for the type

--- instanceOf.js
+++ instanceOf.js
@@ -2,15 +2,21 @@
  //Syntax
 //  obj instanceof Class
 
-class Rabbit{}
-let rabbit = new Rabbit();
-console.log(rabbit instanceof Rabbit);
+// class Rabbit{}
+// let rabbit = new Rabbit();
+// console.log(rabbit instanceof Rabbit);
 
-//  works with constructor functions:
-function Rabbit() {}
-console.log(new Rabbit() instanceof Rabbit);
+// //  works with constructor functions:
+// function Rabbit() {}
+// console.log(new Rabbit() instanceof Rabbit);
 
-//  built-in classes like Array
-let arr = [1,2,3];
-console.log(arr instanceof Array);
-console.log(arr instanceof Object);
+// //  built-in classes like Array
+// let arr = [1,2,3];
+// console.log(arr instanceof Array);
+// console.log(arr instanceof Object);
+
+//Bonus: Object.prototype.toString for the type
+let obj = {};
+
+console.log(obj); // [object Object]
+console.log(obj.toString()); // the same
