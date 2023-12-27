972ab3e54d86bd1573d7db6162ab76f2ffe27449
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 12:16:49 +0530

    Protected properties are usually prefixed with an underscore _

--- class_Inheritance.js
+++ class_Inheritance.js
@@ -90,3 +90,30 @@ class CoffeMachine {
 }
 let coffeMachine = new CoffeMachine(100);
 coffeMachine.waterAmount = 200;
+
+// Protected properties are usually prefixed with an underscore _.
+class CoffeeMachine {
+    _waterAmount = 0;
+  
+    set waterAmount(value) {
+      if (value < 0) {
+        value = 0;
+      }
+      this._waterAmount = value;
+    }
+  
+    get waterAmount() {
+      return this._waterAmount;
+    }
+  
+    constructor(power) {
+      this._power = power;
+    }
+  
+  }
+  
+  // create the coffee machine
+  let coffeeMachine = new CoffeeMachine(100);
+  
+  // add water
+  coffeeMachine.waterAmount = -10; // _waterAmount will become 0, not -10
