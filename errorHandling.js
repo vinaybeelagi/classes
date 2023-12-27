b5539c875250275f6bf5fc1e72a90a17cd05c4f7
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 18:43:26 +0530

    task-Finally or just the code?

--- errorHandling.js
+++ errorHandling.js
@@ -11,3 +11,35 @@ let json = '{"name":"john","age":30}';
 let user = JSON.parse(json);
 console.log(user.name);
 console.log(user.age);
+
+// task-Finally or just the code?
+function f() {
+    try {
+        console.log('start');
+      return "result";
+    } catch (err) {
+      /// ...
+    } finally {
+        console.log('cleanup!');
+    }
+  }
+  
+  f(); // cleanup!
+
+  //when there’s a throw, like here:
+  function f() {
+    try {
+        console.log('start');
+      throw new Error("an error");
+    } catch (err) {
+      // ...
+      if("can't handle the error") {
+        throw err;
+      }
+  
+    } finally {
+        console.log('cleanup!')
+    }
+  }
+  
+  f(); // cleanup!
