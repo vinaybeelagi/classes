24a8dd78469fe54297ebc168ea56564bead4c385
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 13:00:36 +0530

    built-in classes like Array

--- instanceOf.js
+++ instanceOf.js
@@ -9,3 +9,8 @@ console.log(rabbit instanceof Rabbit);
 //  works with constructor functions:
 function Rabbit() {}
 console.log(new Rabbit() instanceof Rabbit);
+
+//  built-in classes like Array
+let arr = [1,2,3];
+console.log(arr instanceof Array);
+console.log(arr instanceof Object);
