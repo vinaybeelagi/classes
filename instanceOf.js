2b982ecd93847d3b419bc4b0cd7f495b9fbe3e02
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-27 13:27:30 +0530

    task-Strange instanceof

--- instanceOf.js
+++ instanceOf.js
@@ -20,3 +20,17 @@ let obj = {};
 
 console.log(obj); // [object Object]
 console.log(obj.toString()); // the same
+
+// task-Strange instanceof
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
+
+/* ANS :instanceof does not care about the function, but rather about its prototype, that it matches against the prototype chain.
+And here a.__proto__ == B.prototype, so instanceof returns true.
+So, by the logic of instanceof, the prototype actually defines the type, not the constructor function.*/
